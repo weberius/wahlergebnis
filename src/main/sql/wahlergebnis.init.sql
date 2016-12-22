b43355ce5cfd63b57f326ba9d32c780e5beabfd2
Wolfram Eberius <eberius@gmail.com>
2016-12-22 05:17:01 +0100

    Fehler 'art' und 'wahl' bearbeiten/ hinzufügen #13

--- src/main/sql/wahlergebnis.init.sql
+++ src/main/sql/wahlergebnis.init.sql
@@ -1,12 +1,15 @@
+DROP TABLE IF EXISTS WAHLDATEN;
 CREATE TABLE WAHLDATEN (
   id         SERIAL PRIMARY KEY, 
   art        varchar(256),
+  wahl       varchar(256),
   datum      timestamp,
   bundesland varchar(2),
   gemeinde   varchar(12),
   modtime    timestamp DEFAULT current_timestamp
 );
 
+DROP TABLE IF EXISTS STIMMBEZIRK;
 CREATE TABLE STIMMBEZIRK (
   id             SERIAL PRIMARY KEY, 
   nr             integer,
@@ -17,6 +20,7 @@ CREATE TABLE STIMMBEZIRK (
   modtime        timestamp DEFAULT current_timestamp
 );
 
+DROP TABLE IF EXISTS ERGEBNIS;
 CREATE TABLE ERGEBNIS (
   id      SERIAL PRIMARY KEY, 
   partei  varchar(512),
@@ -24,11 +28,13 @@ CREATE TABLE ERGEBNIS (
   modtime timestamp DEFAULT current_timestamp
 );
 
+DROP TABLE IF EXISTS ERGEBNIS2STIMMBEZIRK;
 CREATE TABLE ERGEBNIS2STIMMBEZIRK (
   ergebnis    integer, 
   stimmbezirk integer
 );
 
+DROP TABLE IF EXISTS STIMMBEZIRK2WAHLDATEN;
 CREATE TABLE STIMMBEZIRK2WAHLDATEN (
   stimmbezirk integer, 
   wahldaten   integer
