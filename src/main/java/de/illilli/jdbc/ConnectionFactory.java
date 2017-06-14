41065be22b6d07d63e7e13f295972edace5de330
Wolfram Eberius <eberius@gmail.com>
2017-06-14 05:18:19 +0200

    define ConnectionFactory settings in properties #23

--- src/main/java/de/illilli/jdbc/ConnectionFactory.java
+++ src/main/java/de/illilli/jdbc/ConnectionFactory.java
@@ -8,36 +8,20 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import de.illilli.opendata.service.Config;
+
 /**
  * <p>
  * Die ConnectionFactory unterstellt, dass im Tomcat die Verbindung zur
  * Datenbank definiert wurde.
  * </p>
- * <p>
- * Beispiel
- * </p>
- * 
- * <pre>
- * &lt;Resource 
- * 	name="jdbc/wahlergebnis" 
- * 	auth="Container" 
- * 	type="javax.sql.DataSource"
- * 	username="username" 
- * 	password="password" 
- * 	driverClassName="org.postgresql.Driver"
- * 	url="jdbc:postgresql://server:5432/dbname" 
- * 	maxTotal="25" 
- * 	maxIdle="10"
- * 	validationQuery="select 1" /&gt;
- * </pre>
- * 
  */
 public class ConnectionFactory {
 
 	public static Connection getConnection() throws SQLException, NamingException {
 		Context initContext = new InitialContext();
-		Context envContext = (Context) initContext.lookup("java:/comp/env");
-		DataSource ds = (DataSource) envContext.lookup("jdbc/wahlergebnis");
+		Context envContext = (Context) initContext.lookup(Config.getProperty("ConnectionFactory.initContext.lookup"));
+		DataSource ds = (DataSource) envContext.lookup(Config.getProperty("ConnectionFactory.envContext.lookup"));
 		Connection conn = ds.getConnection();
 		return conn;
 	}
