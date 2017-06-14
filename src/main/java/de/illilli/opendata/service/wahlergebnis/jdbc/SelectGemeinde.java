88deb25e86cabeb1cc9fd1e06f7f03909b7f4343
Wolfram Eberius <eberius@gmail.com>
2017-06-14 06:00:18 +0200

    refactoring persistence layer #24

--- src/main/java/de/illilli/opendata/service/wahlergebnis/jdbc/SelectGemeinde.java
+++ src/main/java/de/illilli/opendata/service/wahlergebnis/jdbc/SelectGemeinde.java
@@ -1,29 +1,50 @@
 package de.illilli.opendata.service.wahlergebnis.jdbc;
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.io.InputStream;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-import javax.naming.NamingException;
-
-import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.io.IOUtils;
 
 import de.illilli.jdbc.Select;
 
-public class SelectGemeinde extends Select<GemeindeDTO> {
+public class SelectGemeinde implements Select<GemeindeDTO> {
 
-	private final static String queryString = "/selectGemeinde.sql";
+	private final static String sqlFileName = "/selectGemeinde.sql";
+
+	private String wahl;
+	private String art;
+	private String bundesland;
+	private String gemeinde;
+	private long time;
 
 	public SelectGemeinde(String wahl, String art, String bundesland, String gemeinde, String datum)
-			throws ParseException, SQLException, NamingException, IOException {
+			throws ParseException {
 
-		setQueryString(queryString);
-		long time = new SimpleDateFormat("yyyy-MM-dd").parse(datum).getTime();
-		Object[] params = new Object[] { wahl, art, bundesland, gemeinde, new Timestamp(time) };
-		runSelect(new BeanHandler<GemeindeDTO>(GemeindeDTO.class), params);
+		this.wahl = wahl;
+		this.art = art;
+		this.bundesland = bundesland;
+		this.gemeinde = gemeinde;
+		this.time = new SimpleDateFormat("yyyy-MM-dd").parse(datum).getTime();
 
+	}
+
+	@Override
+	public String getSql() throws IOException {
+		InputStream inputStream = this.getClass().getResourceAsStream(sqlFileName);
+		return IOUtils.toString(inputStream);
+	}
+
+	@Override
+	public Object[] getParameter() {
+		return new Object[] { wahl, art, bundesland, gemeinde, new Timestamp(time) };
+	}
+
+	@Override
+	public Class<GemeindeDTO> getDtoClazz() {
+		return GemeindeDTO.class;
 	}
 
 }
--- src/test/java/de/illilli/opendata/service/wahlergebnis/jdbc/SelectGemeindeTest.java
+++ src/test/java/de/illilli/opendata/service/wahlergebnis/jdbc/SelectGemeindeTest.java
@@ -1,6 +1,7 @@
 package de.illilli.opendata.service.wahlergebnis.jdbc;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.ParseException;
 
@@ -10,7 +11,9 @@ import org.junit.After;
 import org.junit.Before;
 
 import de.illilli.jdbc.ConnectionEnvironment;
+import de.illilli.jdbc.ConnectionFactory;
 import de.illilli.jdbc.Select;
+import de.illilli.jdbc.SelectDao;
 import de.illilli.opendata.service.wahlergebnis.StimmArt;
 import de.illilli.opendata.service.wahlergebnis.Wahl;
 
@@ -26,13 +29,15 @@ public class SelectGemeindeTest {
 
 	public static void main(String[] args) throws SQLException, NamingException, IOException, ParseException {
 		ConnectionEnvironment.setUpConnectionForJndi();
+		Connection conn = ConnectionFactory.getConnection();
+
 		String datum = "2012-05-13";
 		String bundesland = "05";
 		String gemeinde = "05315000";
 		String wahl = Wahl.landtagswahl.name;
 		String art = StimmArt.erststimmen.name;
 		Select<GemeindeDTO> select = new SelectGemeinde(wahl, art, bundesland, gemeinde, datum);
-		GemeindeDTO dto = select.getDbObject();
+		GemeindeDTO dto = new SelectDao<GemeindeDTO>(select, conn).execute();
 		System.out.println(dto.toString());
 	}
 
