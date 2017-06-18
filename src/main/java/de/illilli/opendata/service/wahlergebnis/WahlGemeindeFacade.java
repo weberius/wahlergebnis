93b1fbc6a94b6dca08c170155a901e6de4405d53
Wolfram Eberius <eberius@gmail.com>
2017-06-18 14:18:51 +0200

    refactoring persistence layer #24

--- src/main/java/de/illilli/opendata/service/wahlergebnis/WahlGemeindeFacade.java
+++ src/main/java/de/illilli/opendata/service/wahlergebnis/WahlGemeindeFacade.java
@@ -1,6 +1,7 @@
 package de.illilli.opendata.service.wahlergebnis;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.List;
@@ -9,12 +10,15 @@ import javax.naming.NamingException;
 
 import com.google.gson.Gson;
 
+import de.illilli.jdbc.ConnectionFactory;
 import de.illilli.jdbc.Select;
+import de.illilli.jdbc.SelectDao;
+import de.illilli.jdbc.SelectListDao;
 import de.illilli.opendata.service.Facade;
 import de.illilli.opendata.service.wahlergebnis.jdbc.DTO2Gemeinde;
 import de.illilli.opendata.service.wahlergebnis.jdbc.ErgebnisDTO;
 import de.illilli.opendata.service.wahlergebnis.jdbc.GemeindeDTO;
-import de.illilli.opendata.service.wahlergebnis.jdbc.SelectErgebnis;
+import de.illilli.opendata.service.wahlergebnis.jdbc.SelectErgebnisForGemeinde;
 import de.illilli.opendata.service.wahlergebnis.jdbc.SelectGemeinde;
 import de.illilli.opendata.service.wahlergebnis.model.WahldatenGemeinde;
 
@@ -24,6 +28,7 @@ public class WahlGemeindeFacade implements Facade {
 
 	public WahlGemeindeFacade(String wahl, String bundesland, String gemeinde, String datum, String art)
 			throws SQLException, NamingException, IOException, ParseException {
+		Connection conn = ConnectionFactory.getConnection();
 		// fill wahldaten
 		wahldaten.wahl = wahl;
 		wahldaten.art = art;
@@ -32,10 +37,10 @@ public class WahlGemeindeFacade implements Facade {
 		wahldaten.gemeinde = gemeinde;
 		// fill gemeinde
 		Select<GemeindeDTO> selectGemeinde = new SelectGemeinde(wahl, art, bundesland, gemeinde, datum);
-		GemeindeDTO stimmbezirkDTO = selectGemeinde.getDbObject();
+		GemeindeDTO stimmbezirkDTO = new SelectDao<GemeindeDTO>(selectGemeinde, conn).execute();
 		// fill ergebnis
-		Select<ErgebnisDTO> selectErgebnis = new SelectErgebnis(wahl, art, bundesland, gemeinde, datum);
-		List<ErgebnisDTO> ergebnisDTOList = selectErgebnis.getDbObjectList();
+		Select<ErgebnisDTO> selectErgebnis = new SelectErgebnisForGemeinde(wahl, art, bundesland, gemeinde, datum);
+		List<ErgebnisDTO> ergebnisDTOList = new SelectListDao<ErgebnisDTO>(selectErgebnis, conn).execute();
 
 		wahldaten.wahlergebnis = new DTO2Gemeinde(stimmbezirkDTO, ergebnisDTOList);
 
--- src/main/java/de/illilli/opendata/service/wahlergebnis/WahldatenFacade.java
+++ src/main/java/de/illilli/opendata/service/wahlergebnis/WahldatenFacade.java
@@ -1,6 +1,7 @@
 package de.illilli.opendata.service.wahlergebnis;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +10,9 @@ import javax.naming.NamingException;
 
 import com.google.gson.Gson;
 
+import de.illilli.jdbc.ConnectionFactory;
 import de.illilli.jdbc.Select;
+import de.illilli.jdbc.SelectListDao;
 import de.illilli.opendata.service.Facade;
 import de.illilli.opendata.service.wahlergebnis.jdbc.DTO2Wahldaten;
 import de.illilli.opendata.service.wahlergebnis.jdbc.SelectWahldaten;
@@ -21,8 +24,9 @@ public class WahldatenFacade implements Facade {
 	private List<Wahldaten> wahldatenList = new ArrayList<>();
 
 	public WahldatenFacade() throws SQLException, NamingException, IOException {
+		Connection conn = ConnectionFactory.getConnection();
 		Select<WahldatenDTO> select = new SelectWahldaten();
-		List<WahldatenDTO> dtoList = select.getDbObjectList();
+		List<WahldatenDTO> dtoList = new SelectListDao<WahldatenDTO>(select, conn).execute();
 		for (WahldatenDTO dto : dtoList) {
 			Wahldaten wahldaten = new DTO2Wahldaten(dto);
 			this.wahldatenList.add(wahldaten);
